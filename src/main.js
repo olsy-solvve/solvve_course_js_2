565ede7386b56222acae73ac04c05f6d2a18082f
VolVital <vitalelm61@gmail.com>
2022-08-25 17:59:06 +0300

    commit three

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import PrimeVue from "primevue/config";
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(router);
-
-app.mount("#app");
-import PrimeVue from "primevue/config";
-
-import "./assets/main.css";
-
 app.use(PrimeVue);
+app.mount("#app");
