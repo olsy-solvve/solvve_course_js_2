bfdb4d2d88c677a25dec44df6f4a94727d78001b
vorofikin <nikiforov564@gmail.com>
2022-09-02 23:00:29 +0300

    BACKEND

--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -4,6 +4,7 @@ import GameList from "../views/GameList.vue";
 import GameItem from "../views/GameItem.vue";
 import UserProfile from "../views/UserProfile.vue";
 import RegisterForm from "../views/RegisterForm.vue";
+import PageNotFound from "../views/PageNotFound.vue";
 
 const index = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const index = createRouter({
       path: "/profile",
       name: "profile",
       component: UserProfile,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      composed: PageNotFound
     },
   ],
 });
---
+++ FE/src/views/PageNotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Oops... Page not found</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
