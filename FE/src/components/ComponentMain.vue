cce6e9992a4fb8d9bc3c1378fc4605b40118ae86
VolVital <98488542+volvital@users.noreply.github.com>
2022-09-08 13:35:22 +0300

    Added ComponentMain and GameList

--- FE/src/components/ComponentMain.vue
+++ FE/src/components/ComponentMain.vue
@@ -4,16 +4,19 @@
   <main>
     <div class="grid grid-nogutter surface-section text-800">
       <div
-        class="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center">
+        class="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center"
+      >
         <section>
           <span class="block text-6xl font-bold mb-1"
-            >Create the screens your</span>
+            >Create the screens your</span
+          >
           <div class="text-6xl text-primary font-bold mb-3">
             your visitors deserve to see
           </div>
           <p class="mt-0 mb-4 text-700 line-height-3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
           <pButton
             label="Learn More"
             type="button"
